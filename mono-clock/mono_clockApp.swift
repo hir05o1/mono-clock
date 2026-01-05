ec11cdc94cea9aa2a92dd1175e118b4d4614d05c
hir05o1 <100586810+hir05o1@users.noreply.github.com>
2026-01-06 01:06:42 +0900

    Initial Commit

---
+++ mono-clock/mono_clockApp.swift
@@ -0,0 +1,32 @@
+//
+//  mono_clockApp.swift
+//  mono-clock
+//
+//  Created by hiro on 2026/01/06.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct mono_clockApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ mono-clockTests/mono_clockTests.swift
@@ -0,0 +1,16 @@
+//
+//  mono_clockTests.swift
+//  mono-clockTests
+//
+//  Created by hiro on 2026/01/06.
+//
+
+import Testing
+
+struct mono_clockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
